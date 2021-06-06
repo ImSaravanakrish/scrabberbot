ab975999f12444d31d6ccd75e4f472568e128646
Saravanakrish <68471536+ImSaravanakrish@users.noreply.github.com>
2021-06-06 18:34:15 +0530

    Update __main__.py

--- RSc/__main__.py
+++ RSc/__main__.py
@@ -1,7 +1,7 @@
 from RSc import TOKEN, tbot
 import RSc.events
 
-CHANNEL = -1001379422786
+CHANNEL_ID = -1001379422786
 try:
     tbot.start(bot_token=TOKEN)
 except Exception:
@@ -9,7 +9,7 @@ except Exception:
     exit(1)
 
 async def start_log():
-    await tbot.send_message(-1001379422786, "**Scrapper Started!**")
+    await tbot.send_message(CHANNEL_ID, "**Scrapper Started!**")
 
 
 tbot.loop.run_until_complete(start_log())
